21ca0cfe6db8461b067fca99479051ad9e6eae29
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2025-01-15 21:10:01 +0530

    feat: excerpt & reading time

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,16 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
+import { remarkReadingTime } from './src/remark-plugins/remark-reading-time.mjs';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind({
     applyBaseStyles: false,
   })],
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+  },
   experimental: {
     responsiveImages: true,
   },
