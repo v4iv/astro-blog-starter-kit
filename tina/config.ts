c61d6c42ce935238ad9804faf7326bcf41192cd7
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2025-01-15 23:26:57 +0530

    feat: netlify tinacms config changes

--- tina/config.ts
+++ tina/config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
   // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
   schema: {
     collections: [
-      { ...TinaUserCollection, path: "src/content/users/index.json" },
+      { ...TinaUserCollection, path: "src/content/users" },
       {
         name: "articles",
         label: "Articles",
