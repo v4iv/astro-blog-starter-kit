cac9c34733ca18571a7c55d30e011769228cd2f2
Vaibhav Sharma <vaibhavsharmaatwork@gmail.com>
2025-01-15 19:27:16 +0530

    feat: 404 page

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -8,6 +8,7 @@ export const showDefaultLang = false
 export const ui = {
   en: {
     'navbar.title': 'BLOG',
-    'footer.copyright': `Copyright © ${new Date().getFullYear()} • All Rights Reserved.`
+    'footer.copyright': `Copyright © ${new Date().getFullYear()} • All Rights Reserved.`,
+    '404.pageNotFound': "404 | Page Not Found"
   },
 } as const
